d01e1eb1c8fa102ad8ccf024b45d81ef44b52f6d
Keegan Leary <keeganleary@Keegans-iMac.attlocal.net>
2020-03-24 18:23:55 -0700

    Allowed Host

--- life_cal/settings.py
+++ life_cal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o_2jv#2)y=t6laq)39-7i!8r&@@1vsdtglu+j!lpp)ei6ez_h0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['life-cal.herokuapp.com']
 
 
 # Application definition
